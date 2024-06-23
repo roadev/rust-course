258b2a3a901cec6de3095198ae041cf48b9bad71
Juan Roa <hello@juanroa.dev>
2024-06-23 17:13:08 -0500

    converting age to int and trimming

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@ fn main() {
     println!("Please enter your name: ");
 
     let mut name: String = String::new();
-
     std::io::stdin().read_line(&mut name).unwrap();
+    name = name.trim().to_string();
 
-    println!("Hi, Welcome {}", name);
+    println!("Please enter your age: ");
+
+    let mut age: String = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+
+    // Get age from console
+
+    // Convert age to number
+
+    let age_int: u8 = age.trim().parse().unwrap();
+
+    println!("Hi, Welcome {}, you are {} years old", name, age_int);
 }
